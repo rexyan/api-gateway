3f39087d5a7e1000910af8b853e3083f9feccdd7
runsha.yan <runsha.yan@cyclone-robotics.com>
2021-03-11 14:56:31 +0800

    2021.03.11 v1.0.0, 新增请求限流

--- src/cyclone/core.lua
+++ src/cyclone/core.lua
@@ -1,6 +1,10 @@
 local config = require("cyclone.config")
 local request = require("cyclone.request")
 
+local limit_traffic = require "resty.limit.traffic"
+local limit_conn = require "resty.limit.conn"
+local limit_req = require "resty.limit.req"
+
 local _M = {version = 0.1}
 
 
@@ -53,6 +57,13 @@ function _M.check_custom_rule()
         -- 校验请求头
         local req_headers = request.headers(local_req) or {}
         local header_rules = custom_rule[1]
+        local args_rules = custom_rule[2]
+        local body_rules = custom_rule[3]
+        
+        -- 规则全部为空，则返回 true
+        if table_is_empty(header_rules) and table_is_empty(args_rules) and table_is_empty(body_rules) then
+            return true
+        end
 
         for k, v in pairs(header_rules) do
             if req_headers[k] and req_headers[k] == v then
@@ -62,7 +73,6 @@ function _M.check_custom_rule()
         
         -- 校验 args
         local args = request.get_args(local_req) or {}
-        local args_rules = custom_rule[2]
         for k, v in pairs(args_rules) do
             if args[k] and args[k] == v then
                 args_check_rule_result = true
@@ -71,7 +81,6 @@ function _M.check_custom_rule()
 
         -- 校验 body
         local body = request.get_body(nil, local_req) or {}
-        local body_rules = custom_rule[3]
         for k, v in pairs(body_rules) do
             if body[k] and body[k] == v then
                 body_check_rule_result = true
@@ -83,16 +92,74 @@ function _M.check_custom_rule()
     end
 end
 
+-- 验证 jwt token
+function _M.check_jwt_token(token)
+    -- TODO 校验 Token
+    return true
+end
 
 -- CAS Token 转换用户信息
-function _M.convert_cas_token()
-    -- 判断哪些链接需要进行认证转换，不进行的直接放行
+function _M.cyclone_auth()
+    -- 判断哪些链接需要进行认证
     local no_auth_list = config.get_no_auth_list()
     local current_req_url = ngx.req
 
     if not is_in_array(current_req_url, no_auth_list) then
-        -- 获取用户信息
-        
+        -- 验证 jwt token
+        return _M.check_jwt_token()
+    end
+
+    return true
+end
+
+
+-- 请求限流
+function _M.limit_req_conn()
+    -- 限制请求速率
+    -- 限制并发连接数
+    local lim1, err = limit_req.new("limit_req_store", 300, 200)   -- 限制请求速率。每秒的速率设置为 300。如果超过 300 但小于 500（300+200 计算得到），就需要排队等候；如果超过 500，就会直接拒绝。
+    local lim2, err = limit_conn.new("limit_conn_store", 300, 500, 0.5)   -- 限制并发连接数。限制一个 ip 客户端最大并发请求个数， 第二个参数为漏桶的桶容量，最后一个参数其实是你要预估这些并发（或者说单个请求）要处理多久
+    
+    local limiters = {lim1, lim2}
+    local host = ngx.var.host
+    local client = ngx.var.binary_remote_addr
+    local keys = {host, client}
+    local states = {}
+
+    local delay, err = limit_traffic.combine(limiters, keys, states)
+    if not delay then
+        if err == "rejected" then
+            ngx.log(ngx.INFO, "request rejected")
+            return ngx.exit(503)
+        end
+        ngx.log(ngx.ERR, "failed to limit traffic: ", err)
+        return ngx.exit(500)
+    end
+
+    if lim2:is_committed() then
+        local ctx = ngx.ctx
+        ctx.limit_conn = lim2
+        ctx.limit_conn_key = keys[2]
+    end
+
+    if delay >= 0.001 then
+        ngx.sleep(delay)
+    end
+
+end
+
+
+-- 释放请求限流计数
+function _M.release_limit_req_conn()
+    local ctx = ngx.ctx
+    local lim = ctx.limit_conn
+    if lim then
+        local key = ctx.limit_conn_key
+        local conn, err = lim:leaving(key, 0.5)
+        if not conn then
+            ngx.log(ngx.ERR,"failed to record the connection leaving ", "request: ", err)
+            return
+        end
     end
 end
 
--- src/init.lua
+++ src/init.lua
@@ -20,15 +20,35 @@ end
 function _M.http_init_worker()
 end
 
+local function fake_fetch()
+    ngx.ctx.ip = "127.0.0.1"
+    ngx.ctx.port = 5000
+end
 
 function _M.http_access_phase()
-    -- 校验 IP, Host, 自定义规则 
-    if not cyclone_core.check_remote_addr() or not cyclone_core.check_host() or not cyclone_core.check_custom_rule() then
+    -- 请求限流
+    cyclone_core.limit_req_conn()
+    fake_fetch()
+
+    -- 校验 IP
+    if not cyclone_core.check_remote_addr() then
         cyclone_util.http_forbidden()
     end
 
-    -- CAS Token 转换用户信息
-    cyclone_core.convert_cas_token()
+    -- 校验 Host
+    if not cyclone_core.check_host() then
+        cyclone_util.http_forbidden()
+    end
+
+    -- 校验 自定义规则 
+    if not cyclone_core.check_custom_rule() then
+        cyclone_util.http_forbidden()
+    end
+    
+    -- 校验 Token
+    if not cyclone_core.cyclone_auth() then
+        cyclone_util.http_forbidden()
+    end
 
 end
 
@@ -46,7 +66,8 @@ end
 
 function _M.http_log_phase()
     if ngx.ctx then
-        -- do something
+        -- 释放请求限流计数
+        cyclone_core.release_limit_req_conn()
     end
 end
 
@@ -57,6 +78,7 @@ function _M.http_ssl_phase()
 end
 
 function _M.http_balancer_phase()
+    -- 负载均衡
     local ok, err = balancer.set_current_peer(ngx.ctx.ip, ngx.ctx.port)
     if not ok then
         ngx.log(ngx.ERR, "failed to set the current peer: ", err)
